ba98ab252a1507e627b8dc9d628057db2a94547f
Nguyen Duc Thang <aloducthang@gmail.com>
2023-11-25 14:01:21 +0700

    Xây dựng Logic phần Header #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,17 +2,18 @@ import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Image from '../Image';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/915439d04dfd7b7a9dd518cc10eb4366.jpeg?x-expires=1700978400&x-signature=q32R5nUc8mn8jdZAzCzEW2vgE94%3D"
                 alt=""
-            ></img>
+            />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
                     <span>Nguyễn Hoàng Ngân</span>
